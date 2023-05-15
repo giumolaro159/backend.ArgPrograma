84049f5a16829bf3bd446b90b270038c7515bdff
Giuliana Molaro <123484973+giumolaro159@users.noreply.github.com>
2023-05-14 21:15:30 -0300

    Delete demo.backend directory

--- demo.backend/demo/README.txt
+++
@@ -1,1 +0,0 @@
-Este proyecto está licenciado bajo la Apache License 2.0. Ver el archivo LICENSE.txt para más detalles.
--- demo.backend/demo/src/main/java/com/example/demo/security/enums/RollNombre.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.security.enums;
-
-public enum RollNombre {
-    ROLE_ADMIN, ROLE_USER
-    
-}
